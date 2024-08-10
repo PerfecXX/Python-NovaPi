9ce051b65b4eb096d1e830d9f90b94200f753bf4
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-08-11 02:03:24 +0700

    Tools for NovaPi

---
+++ tools/nvptool.py
@@ -0,0 +1,8 @@
+"""
+Author : Teeraphat Kullanankanjana
+Date : 24/06/2567 
+Version : 0.0.1
+Description : Tools for MakeBlock NovaPi Board
+Copyright (C) 2024 Teeraphat Kullanankanjana. All rights reserved.
+"""
+
